18bb21e13dc2c807ae6508336703ab2b60e0a2d5
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-19 15:41:22 +0300

    Complete lab assessment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+(2, 'Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+(3, 'Tabitha', 7, 'F', 'Black', 'Friendly', 1),
+(4, 'Sergeant Brown', 12, 'M', 'Brown', 'Calm', 0),
+(5, 'Melissa', 6, 'F', 'White', 'Playful', 1),
+(6, 'Grinch', 8, 'M', 'Black', 'Grumpy', 0),
+(7, 'Wendy', 4, 'F', 'Brown', 'Curious', 1),
+(8, NULL, 3, 'F', 'Brown', 'Playful', 1);
